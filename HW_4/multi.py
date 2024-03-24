059ec8f8f3f73a8e32aa97c52e4966915d59ac7f
Iwan201087 <iwan201087@gmail.com>
2024-03-25 01:10:42 +0300

    Домашняя работа к семинару №4 по Flask

---
+++ HW_4/multi.py
@@ -0,0 +1,39 @@
+import sys
+import requests
+from multiprocessing import Process, Pool
+import time
+
+
+def get_file(url, start_time):
+    response = requests.get(url)
+    if response.status_code == 200:
+        filename = url.split('/')[-1]
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+            print(f"Downloaded {url} in {time.time() - start_time:.2f} seconds")
+    else:
+        print(f"Failed to download {url}: HTTP status code {response.status_code}")
+
+
+def main(urls, start_time):
+    processes = []
+    for url in urls:
+        process = Process(target=get_file, args=(url,start_time))
+        processes.append(process)
+        process.start()
+    for process in processes:
+        process.join()
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    if len(sys.argv) > 1:
+        urls = sys.argv[1:]
+        main(urls,start_time)
+    else:
+        urls = [
+            'https://images.pexels.com/photos/19597529/pexels-photo-19597529.jpeg',
+            'https://images.pexels.com/photos/19808874/pexels-photo-19808874.jpeg',
+            'https://images.pexels.com/photos/12801958/pexels-photo-12801958.jpeg'
+        ]
+        main(urls, start_time)
